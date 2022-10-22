7252128701bff41d69ac20e98af03d4b5b86619e
Simon <moawadsimon@gmail.com>
2022-10-22 18:32:44 +0300

    Fully implemented the Splash Screen with a dummy intent for later use

--- app/src/main/java/com/csc498g/bliss/SplashScreenActivity.java
+++ app/src/main/java/com/csc498g/bliss/SplashScreenActivity.java
@@ -2,13 +2,30 @@ package com.csc498g.bliss;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import java.util.Objects;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(() -> {
+
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, 5000);
     }
 }
