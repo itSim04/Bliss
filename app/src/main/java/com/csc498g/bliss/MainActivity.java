749dacbe3ca077eaa0f3b3b96457b0c1b17fae7d
Simon Mouawad <43381860+itSim04@users.noreply.github.com>
2022-10-23 14:26:02 +0300

    Created the register activity layout

--- app/src/main/java/com/csc498g/bliss/MainActivity.java
+++ app/src/main/java/com/csc498g/bliss/MainActivity.java
@@ -3,12 +3,20 @@ package com.csc498g.bliss;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        setContentView(R.layout.activity_login);
     }
 }
