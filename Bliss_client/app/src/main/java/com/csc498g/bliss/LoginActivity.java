68245551b8d6aa503d42adb6237ed6472a3b42ea
elitek7 <elite.keryakos@hotmail.com>
2022-10-24 19:49:55 +0300

    created login and register activities

---
+++ Bliss_client/app/src/main/java/com/csc498g/bliss/LoginActivity.java
@@ -0,0 +1,46 @@
+package com.csc498g.bliss;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import java.util.Objects;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        setContentView(R.layout.activity_login);
+    }
+
+    public void signIn(View v) {
+        EditText username = (EditText) findViewById(R.id.nameEdt);
+        EditText password = (EditText) findViewById(R.id.passwordEdt);
+
+        String username_input = username.getText().toString();
+        String password_input = password.getText().toString();
+        if(username_input.equals("") || password_input.equals(""))
+        {    //Making sure the user inputs both
+            Toast.makeText(getApplicationContext(), "Missing entries.", Toast.LENGTH_LONG).show();
+        }
+        else
+        {
+            Intent i = new Intent(getApplicationContext(), FeedActivity.class);
+            startActivity(i);
+        }
+    }
+
+    public void signUp(View v) {
+        Intent i = new Intent(getApplicationContext(), RegisterActivity.class);
+        startActivity(i);
+    }
+}
