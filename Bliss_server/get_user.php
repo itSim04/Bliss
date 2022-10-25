c6e5139d2bad9877c94dc2d71895de843a36f65a
elitek7 <elite.keryakos@hotmail.com>
2022-10-25 19:33:01 +0300

    Created the get_user api

--- Bliss_server/get_user.php
+++ Bliss_server/get_user.php
@@ -3,19 +3,16 @@
 require 'connection.php';
 
 try {
-	$query = $mysqli->prepare("SELECT follows.user_id1 FROM follows WHERE follows.user_id2 = ?");
+	$query = $mysqli->prepare("SELECT * FROM users WHERE user_id = ?");
 	$query->bind_param("i", $_POST["user_id"]);
 	$query->execute();
 	$result = $query->get_result();
 
-	$tweets = [];
-	while($row = mysqli_fetch_assoc($result)) {
-		$tweets[] = $row;
-	}
-
+	$row = mysqli_fetch_assoc($result);
+	
 	$output["success"] = true;
 	$output["error"] = 0;
-	$output["result"] = $tweets;
+	$output["result"] = $row;
 }
 
 catch(Exception $e) {
