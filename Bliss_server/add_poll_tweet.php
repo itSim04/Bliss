6f2a0f6472644b5cef0d340758afcd79458a26c7
elitek7 <elite.keryakos@hotmail.com>
2022-10-25 18:44:15 +0300

    Created the add_poll_tweet api

---
+++ Bliss_server/add_poll_tweet.php
@@ -0,0 +1,21 @@
+<?php
+
+require 'add_tweet.php';
+
+try {
+	$query = $mysqli->prepare("INSERT INTO polltweets (tweet_id, prompt. option1, option2, option3, option4) VALUES (?, ?, ?, ?, ?, ?)");
+	$query->bind_param("isssss", $output["inserted_id"], $_POST["content"], $_POST["option1"], $_POST["option2"], $_POST["option3"], $_POST["option4"]);
+	$query->execute();
+	$output["success"] = true;
+	$output["error"] = 0;
+}
+
+catch(Exception $e) {
+	$output["success"] = false;
+	$output["error"] = $e->getMessage();
+	$query = $mysqli->prepare("DELETE FROM tweets WHERE tweet_id = ?");
+	$query->bind_param("i", $output["inserted_id"]);
+	$query->execute();
+}
+
+print_r($output);
