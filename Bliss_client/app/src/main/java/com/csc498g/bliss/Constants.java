378c5560cf44ac8032ac919fbaaafe662d293500
Simon Mouawad <43381860+itSim04@users.noreply.github.com>
2022-10-27 18:50:38 +0300

    Documented all APIs

--- Bliss_client/app/src/main/java/com/csc498g/bliss/Constants.java
+++ Bliss_client/app/src/main/java/com/csc498g/bliss/Constants.java
@@ -2,7 +2,7 @@ package com.csc498g.bliss;
 
 public class Constants {
 
-    class users {
+    class Users {
 
         public static final String USER_ID = "user_id";
         public static final String USERNAME = "username";
@@ -15,7 +15,7 @@ public class Constants {
 
     }
 
-    class gems {
+    class Gems {
 
         public static final String GEM_ID = "gem_id";
         public static final String MINE_DATE = "mine_id";
@@ -26,7 +26,7 @@ public class Constants {
 
     }
 
-    class remines {
+    class Remines {
 
         public static final String USER_ID = "user_id";
         public static final String GEM_ID = "gem_id";
@@ -34,13 +34,13 @@ public class Constants {
 
     }
 
-    class follows {
+    class Follows {
 
         public static final String USER_ID1 = "user_id1";
         public static final String USER_ID2 = "user_id2";
     }
 
-    class diamonds {
+    class Diamonds {
 
         public static final String USER_ID = "user_id";
         public static final String GEM_ID = "gem_id";
@@ -48,12 +48,41 @@ public class Constants {
 
     }
 
-    class answers {
+    class Answers {
 
         public static final String USER_ID = "user_id";
         public static final String GEM_ID = "gem_id";
         public static final String ANSWER_DATE = "answer_date";
         public static final String OPTION_CHOSEN = "option_chosen";
+
+
+    }
+
+    class APIs {
+
+        public static final String ADD_GEM = "add_gem";
+        public static final String ADD_USER = "add_user";
+        public static final String AUTHENTICATE_LOGIN = "authenticate_login";
+        public static final String DIAMOND_GEM = "diamond_gem";
+        public static final String FOLLOW_USER = "follow_user";
+        public static final String GET_ALL_DIAMONDS_ON_GEM = "get_all_diamonds_on_gem"
+        public static final String GET_ALL_GEMS = "get_all_gems";
+        public static final String GET_ALL_IMAGE_GEMS = "get_all_image_gems";
+        public static final String GET_ALL_POLL_GEMS = "get_all_poll_gems";
+        public static final String GET_ALL_TEXT_GEMS = "get_all_text_gems";
+        public static final String GET_ALL_USERS = "get_all_users";
+        public static final String GET_ALL_VIDEO_GEMS = "get_all_video_gems";
+        public static final String GET_FOLLOWERS = "get_followers";
+        public static final String GET_FOLLOWINGS = "get_followings";
+        public static final String GET_USER = "get_user";
+        public static final String IS_EMAIL_AVAILABLE = "is_email_available";
+        public static final String IS_USERNAME_AVAILABLE = "is_username_available";
+
+    }
+
+    class URL {
+
+        public static final String MASTER_URL = "http://192.168.0.103/Bliss/Bliss_server/";
 
 
     }
