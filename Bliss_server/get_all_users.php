5e577c993f860346f4077fc73b7151724c9b3b1d
Simon Mouawad <43381860+itSim04@users.noreply.github.com>
2022-10-27 16:12:57 +0300

    Created the get_all_users api

---
+++ Bliss_server/get_all_users.php
@@ -0,0 +1,26 @@
+<?php
+
+require 'connection.php';
+
+try {
+	$query = $mysqli->prepare("SELECT * FROM users");
+	$query->execute();
+	$result = $query->get_result();
+
+	$tweets = [];
+	while($row = mysqli_fetch_assoc($result)) {
+		$tweets[] = $row;
+	}
+
+	$output["success"] = true;
+	$output["error"] = 0;
+	$output["result"] = $tweets;
+}
+
+catch(Exception $e) {
+	$output["success"] = false;
+	$output["result"] = 0;
+	$output["error"] = $e->getMessage();
+}
+
+echo json_encode($output);
