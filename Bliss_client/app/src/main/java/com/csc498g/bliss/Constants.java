1c39e680c9622fff15d37c31f3da230d815758f8
Simon Mouawad <43381860+itSim04@users.noreply.github.com>
2022-10-27 18:36:24 +0300

    Added database columns names to Constants file

--- Bliss_client/app/src/main/java/com/csc498g/bliss/Constants.java
+++ Bliss_client/app/src/main/java/com/csc498g/bliss/Constants.java
@@ -15,6 +15,49 @@ public class Constants {
 
     }
 
-    class 
+    class gems {
+
+        public static final String GEM_ID = "gem_id";
+        public static final String MINE_DATE = "mine_id";
+        public static final String EDIT_DATE = "edit_date";
+        public static final String CONTENT = "content";
+        public static final String TYPE = "type";
+        public static final String OWNER_ID = "owner_id";
+
+    }
+
+    class remines {
+
+        public static final String USER_ID = "user_id";
+        public static final String GEM_ID = "gem_id";
+        public static final String REMINE_DATE = "remine_date";
+
+    }
+
+    class follows {
+
+        public static final String USER_ID1 = "user_id1";
+        public static final String USER_ID2 = "user_id2";
+    }
+
+    class diamonds {
+
+        public static final String USER_ID = "user_id";
+        public static final String GEM_ID = "gem_id";
+        public static final String DIAMOND_DATE = "diamond_date";
+
+    }
+
+    class answers {
+
+        public static final String USER_ID = "user_id";
+        public static final String GEM_ID = "gem_id";
+        public static final String ANSWER_DATE = "answer_date";
+        public static final String OPTION_CHOSEN = "option_chosen";
+
+
+    }
+
+
 
 }
