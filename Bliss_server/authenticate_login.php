3668d854a13a7dfe9ce3d5e1203c13e01276d303
Simon Mouawad <43381860+itSim04@users.noreply.github.com>
2022-10-29 14:09:13 +0300

    Handled Errors in authenticate_login.php

--- Bliss_server/authenticate_login.php
+++ Bliss_server/authenticate_login.php
@@ -2,23 +2,29 @@
 
 require 'connection.php';
 
-try {
+if (array_key_exists("username", $_POST) && array_key_exists("password", $_POST)) {
+	$username = $_POST["username"];
+	$password =  $_POST["password"];
+	try {
 
-	$query = $mysqli->prepare("SELECT * FROM users WHERE username = ? AND password = ?");
-	$query->bind_param("ss", $_POST["username"], $_POST["password"]);
-	$query->execute();
-	$result = $query->get_result();
 
-	$row = mysqli_fetch_assoc($result);
+		$query = $mysqli->prepare("SELECT * FROM users WHERE username = ? AND password = ?");
+		$query->bind_param("ss", $username, $password);
+		$query->execute();
+		$result = $query->get_result();
 
-	$output["success"] = true;
-	$output["error"] = 0;
-}
+		$row = mysqli_fetch_assoc($result);
 
-catch(Exception $e) {
+		$output["success"] = true;
+		$output["error"] = 0;
+	} catch (Exception $e) {
 
+		$output["success"] = false;
+		$output["error"] = $e->getMessage();
+	}
+} else {
 	$output["success"] = false;
-	$output["error"] = $e->getMessage();
+	$output["error"]   = "Missing Attributes";
 }
 
 $output["is_authenticated"] = mysqli_num_rows($result) > 0;
