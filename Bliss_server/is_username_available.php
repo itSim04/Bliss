ff28cc1b4dc86b1bf8087965fa76a4c8432a2099
Simon Mouawad <43381860+itSim04@users.noreply.github.com>
2022-10-29 14:28:09 +0300

    Handled errors in availability APIs

--- Bliss_server/is_username_available.php
+++ Bliss_server/is_username_available.php
@@ -2,26 +2,35 @@
 
 require 'connection.php';
 
-try {
+if (array_key_exists("username", $_POST)) {
 
-	$query = $mysqli->prepare("SELECT * FROM users WHERE username = ?");
-	$query->bind_param("s", $_POST["username"]);
-	$query->execute();
-	$result = $query->get_result();
+	try {
 
-	$row = mysqli_fetch_assoc($result);
+		$query = $mysqli->prepare("SELECT * FROM users WHERE username = ?");
+		$query->bind_param("s", $_POST["username"]);
+		$query->execute();
+		$result = $query->get_result();
 
-	$output["success"] = true;
-	$output["error"] = 0;
-	$output["available"] = mysqli_num_rows($result) <= 0;
-}
+		$row = mysqli_fetch_assoc($result);
 
-catch(Exception $e) {
+		$output["success"] = true;
+		$output["error"] = 0;
+		$output["available"] = mysqli_num_rows($result) <= 0;
+
+	} catch (Exception $e) {
+
+		$output["success"] = false;
+		$output["error"] = $e->getMessage();
+		$output["is_available"] = false;
+
+	}
 	
+} else {
+
 	$output["success"] = false;
-	$output["error"] = $e->getMessage();
-	$output["is_available"] = -1S;
-	
+	$output["query_result"] = null;
+	$output["error"] = "Missing User ID";
+
 }
 
 
