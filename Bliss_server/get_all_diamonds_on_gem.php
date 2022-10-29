2ba386a5e67204875dfa8c44810ad0079aa29d5e
Simon Mouawad <43381860+itSim04@users.noreply.github.com>
2022-10-29 14:23:35 +0300

    Handled errors in get_all_diamonds_on_gem.php

--- Bliss_server/get_all_diamonds_on_gem.php
+++ Bliss_server/get_all_diamonds_on_gem.php
@@ -2,27 +2,39 @@
 
 require 'connection.php';
 
-try {
-	$query = $mysqli->prepare("SELECT DISTINCT users.user_id, users.username, diamonds.diamond_date FROM diamonds, gems, users WHERE diamonds.gem_id = ? AND diamonds.user_id = users.user_id AND diamonds.user_id");
-	$query->bind_param("i", $_POST["gem_id"]);
-	$query->execute();
-	$result = $query->get_result();
+if (array_key_exists("gem_id", $_POST)) {
 
-	$gems = [];
-	while($row = mysqli_fetch_assoc($result)) {
-		$gems[] = $row;
+	try {
+
+		$query = $mysqli->prepare("SELECT DISTINCT users.user_id, users.username, diamonds.diamond_date FROM diamonds, gems, users WHERE diamonds.gem_id = ? AND diamonds.user_id = users.user_id AND diamonds.user_id");
+		$query->bind_param("i", $gem_id);
+		$query->execute();
+		$result = $query->get_result();
+
+		$gems = [];
+
+		while ($row = mysqli_fetch_assoc($result)) {
+
+			$gems[] = $row;
+		}
+
+
+		$output["success"] = true;
+		$output["error"] = 0;
+		$output["query_result"] = $gems;
+
+	} catch (Exception $e) {
+
+		$output["success"] = false;
+		$output["query_result"] = 0;
+		$output["error"] = $e->getMessage();
 	}
 
-	$output["success"] = true;
-	$output["error"] = 0;
-	$output["query_result"] = $gems;
-}
+} else {
 
-catch(Exception $e) {
-	
 	$output["success"] = false;
-	$output["query_result"] = 0;
-	$output["error"] = $e->getMessage();
+	$output["error"]   = "Missing Attributes";
+	
 }
 
 echo json_encode($output);
