7a83f7419f045fddd230157953122a2517f82588
Elite Keryakos <elite.keryakos@hotmail.com>
2022-11-01 14:32:46 +0200

    Added a get_all_gems_by_user API

---
+++ Bliss_server/get_all_gems_by_user.php
@@ -0,0 +1,47 @@
+<?php
+
+require 'connection.php';
+
+if(array_key_exists("owner_id", $_GET)) {
+$owner_id = $_GET["owner_id"];
+try {
+
+	$query = $mysqli->prepare("SELECT * FROM gems WHERE owner_id = ?");
+	$query->bind_param("i", $owner_id);
+	$query->execute();
+	$result = $query->get_result();
+	
+	$gems = [];
+	$users = [];
+
+	while($row = mysqli_fetch_assoc($result)) {
+
+		
+		$gems[] = $row;
+		$subquery .= " user_id = " . $row["owner_id"] . " ||";
+
+	}
+
+
+	$output["success"] = true;
+	$output["error"] = 0;
+
+	$result = [];                       
+	$result["gem"] = $gems;
+	$output["query_result"] = $result;
+
+}
+
+catch(Exception $e) {
+	$output["success"] = false;
+	$output["query_result"] = null;
+	$output["error"] = $e->getMessage();
+}} else {
+
+$output["success"] = false;
+	$output["query_result"] = null;
+	$output["error"] = "Missing User ID";
+
+}
+
+echo json_encode($output);
